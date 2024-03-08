73de8b26d13fa7b2af17c78c634ba993c2250421
CanOguzz <mustafacanoguz@gmail.com>
2024-03-08 15:53:33 +0300

    Default code deleted from index.html and App.js Height and background properties added in intex.css Icons added into Assets folder RandomQuote.jsx component functions added RandomQuote.css properties added

---
+++ src/RandomQuote/RandomQuote.jsx
@@ -0,0 +1,46 @@
+import React, {useState}from 'react'
+import './RandomQuote.css'
+import twitter_icon from '../Assets/twittericon.png'
+import reflesh_icon from '../Assets/refleshicon.png'
+
+export const RandomQuote = () => {
+
+    let quotes=[]
+
+    async function loadQuotes(){
+        const response = await fetch("https://type.fit/api/quotes")
+        quotes = await response.json()
+    }
+
+    
+
+    const [quote, setQuote] = useState({
+        text:"texttext",
+        author:"authorauthor"
+    });  
+
+    const random =() => {  
+        const select = quotes[Math.floor(Math.random()*quotes.length)]
+        setQuote(select)
+    }
+    
+    loadQuotes()
+
+    return (
+
+   
+    <div id="quote-box">
+        <div id="text">{quote.text}</div>
+        <div>
+            <div className='line'></div>
+            <div className='bottom'> 
+            <div id="author">{quote.author}</div>
+            <button type="button" id="new-quote">
+                <img src={reflesh_icon} alt='refleshimg' onClick={()=>{random()}}></img>
+                {/* <img src={twitter_icon} alt=''></img> */}
+            </button>
+            </div>
+        </div>
+    </div>
+  )
+}
